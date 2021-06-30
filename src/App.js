ed780bb98f35c05aab6a55804a2e3edf280fea9e
lucg23 <lucasferreira.23m@gmail.com>
2021-06-30 20:41:32 -0300

    api configurada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
 
-function App() {
+
+
+const App = () => {
+  useEffect(()=>{
+   const loadAll = async () =>{
+    // Pegando a lista TOTAL 
+    let list = await Tmdb.getHomeList();
+    console.log(list);
+  }
+  
+  loadAll();
+  
+
+   }, []);
+    
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Olá Mundo
     </div>
   );
 }
