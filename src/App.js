85cd7b270cb98e564cad86901b65a937d0e1f880
lucg23 <lucasferreira.23m@gmail.com>
2021-06-30 21:30:15 -0300

    buscando imagens da api e estilizando a página

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import MovieRow from './components/MovieRow';
 import Tmdb from './Tmdb';
 
 
 
 const App = () => {
+  
+  const [movieList, setMovieList] = useState([])
+  
+  
   useEffect(()=>{
    const loadAll = async () =>{
     // Pegando a lista TOTAL 
     let list = await Tmdb.getHomeList();
-    console.log(list);
+    setMovieList(list);
   }
   
   loadAll();
@@ -19,8 +24,12 @@ const App = () => {
     
   
   return (
-    <div className="App">
-      Olá Mundo
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) =>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
